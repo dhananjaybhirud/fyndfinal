79749d4b4b51ebd7855e33463f84eb59b0f9c28c
dhananjaybhirud <dhananjaybhirud@gmail.com>
2020-09-22 13:28:59 +0530

    - code fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
-import countries  from './data.json';
+import { Component, OnInit } from '@angular/core';
+import birthDays from './data.json';
+import * as moment from 'moment';
+import * as _ from 'underscore';
+
+interface IEmpBirthday {
+  name: string;
+  birthday: any;
+  date?: string;
+  month?: string;
+  year?: string;
+  shName?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fynd';
-  public countryList:{name:string, birthday:Date}[] = countries;
+  public birthDayList: any[] = [];
+
+  ngOnInit() {
+
+    birthDays.forEach((emp: IEmpBirthday) => {
+      emp.birthday = moment(emp.birthday, 'MM-DD-YYYY');
+      emp.date = emp.birthday.format('ddd');
+      emp.month = emp.birthday.get('month');
+      emp.year = emp.birthday.get('year');
+      emp.shName = this.getShName(emp.name);
+    });
+
+    birthDays.sort((a: IEmpBirthday, b: IEmpBirthday) => {
+      return a.birthday.valueOf() - b.birthday.valueOf();
+    });
+
+    this.birthDayList = _.groupBy(birthDays, (emp: IEmpBirthday) => emp.birthday.format('MM-DD-YYYY'));
+    this.birthDayList = Object.entries(this.birthDayList);
+
+    console.log(this.birthDayList);
+  }
+
+  getShName(name: string) {
+    let shName = name.split(' ');
+    shName = shName.map((val: string) => val[0]);
+    return shName.join('');
+  }
+
+  getDayName(date: string) {
+    return moment(date, 'MM-DD-YYYY').format('ddd');
+  }
 }
